684730851da413ffac7595ef37bbbafa4cd2db53
marketaboh <bohackovama@gmail.com>
2025-02-03 14:52:20 +0100

    vytvoreni uvodu a první fuknce na kontrolu vstupu

---
+++ projekt_2.py
@@ -0,0 +1,39 @@
+"""
+projekt_2.py: druhý projekt do Engeto Online Python Akademie
+
+author: Markéta Boháčková
+email: bohackovama@gmail.com
+"""
+import random
+
+print("Hi there!")
+print("-----------------------------------------------")
+print("I've generated a random 4 digit number for you.")
+print("Let's play a bulls and cows game.")
+print("-----------------------------------------------")
+
+def check_is_number(input):
+    if input.isdigit() and len(input) == 4 and input[0] != '0':
+        return input
+    elif input[0] == '0':
+        print("Please enter a 4 digit number without leading zeros.")
+        return False
+    else:
+        print("Please enter a 4 digit number.")
+        return False
+    
+# generate a random 4 digit number
+number = random.randint(1000, 9999)
+print(number)
+
+# Loop until a valid input is entered
+while True:
+    user_input = input("Enter a number: ")
+    if check_is_number(user_input):
+        break  # Exit the loop if input is valid
+
+
+print(user_input)
+#user_input = int(input)
+
+print("-----------------------------------------------")
