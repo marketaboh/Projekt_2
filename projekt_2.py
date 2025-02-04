239cad36446aa5305d14b379224de96d2153ad89
marketaboh <bohackovama@gmail.com>
2025-02-04 13:38:06 +0100

    uprava na funkci bull cow

--- projekt_2.py
+++ projekt_2.py
@@ -6,13 +6,12 @@ email: bohackovama@gmail.com
 """
 import random
 
-print("Hi there!")
-print("-" * 50)
-print("I've generated a random 4 digit number for you.")
-print("Let's play a bulls and cows game.")
-print("-" * 50)
+def check_input(input:str)->bool:
+    """ function checks if the input is a 4 digit number with unique digits 
+    and returns True if the input is valid, otherwise returns False 
+    and prints an error message 
+    input: str ->bool """
 
-def check_input(input):
     if input.isdigit() and len(set(input)) == 4 and input[0] != '0':
         return True
     elif input.isdigit() and input[0] == '0':
@@ -36,39 +35,48 @@ def generate_unique_digit_number():
         if len(set(str_number)) == 4:  # Check if all digits are unique
             return number
 
-# generate a random 4 digit number
-random_number = generate_unique_digit_number()
-print("Random číslo: ",random_number)
+def play_bulls_and_cows():
+    """ function plays a bulls and cows game.
+    The player has to guess a 4 digit number with unique digits. 
+    The game provides feedback in the form of bulls and cows."""
 
-# Loop until a valid input is entered
-while True:
-    user_input = input("Enter a number: ")
-    if check_input(user_input):
-        break  # Exit the loop if input is valid
-print("-" * 50)
+    print("Hi there!")
+    print("-" * 50)
+    print("I've generated a random 4 digit number for you.")
+    print("Let's play a bulls and cows game.")
+    print("-" * 50)
+    random_number = generate_unique_digit_number() # Generate a random 4 digit number
+    random_number = str(random_number)
+    print("Random číslo: ",random_number)
+    attempts = 1 # Number of guesses
 
-print(user_input)
-random_number = str(random_number)
-attempts = 1
-while user_input != random_number:
-    bulls = 0
-    cows = 0
-    attempts += 1
-    if user_input != random_number:
+    # Loop until a valid input is entered 
+    while True:
+        user_input = input("Enter a number: ")
+        if check_input(user_input):
+            break  # Exit the loop if input is valid
+    print("-" * 50)
+    
+    while user_input != random_number:
+        bulls = 0
+        cows = 0
+        attempts += 1
         for i in range(4):
             if user_input[i] == random_number[i]:
                 bulls += 1
             elif user_input[i] in random_number:
                 cows += 1
-        print(f"{bulls} bulls, {cows} cows")
+        print(f"{bulls} bull{'s' if bulls != 1 else ''}, {cows} cow{'s' if cows != 1 else ''}")
         while True: 
             user_input = input("Enter a number: ")
             print("-" * 50)
             if check_input(user_input):
                 break
         continue      
-    else:
-        break
     
-print(f"Correct, you've guessed the right number in {attempts} guesses!") 
+    print(f"Correct, you've guessed the right number in {attempts} guess{'es' if attempts != 1 else ''}!")
+    print("-" * 50)
+    print("That's amazing!")
 
+if __name__ == "__main__":
+    play_bulls_and_cows()
